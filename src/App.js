f45886a082dc50d89747cbeb26a330700af39fa8
anthony.bechay <anthonybechay@hotmail.com>
2019-10-09 15:32:22 +0300

    add spotify credentials

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('https://accounts.spotify.com/authorize?client_id=928172297b4247e49cd958cf8552b6e8&response_type=code&redirect_uri=https://www.littlebangkoklb.com')
       .then(res => res.json())
       .then((data) => {
         this.setState({ contacts: data })
